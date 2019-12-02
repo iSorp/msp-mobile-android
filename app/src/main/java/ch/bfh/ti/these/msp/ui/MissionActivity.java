e8eb39d9ed01a1876ba3b862eb9d3ba753f5f347
Samuel Ackermann <saemi.ackermann@gmail.com>
2019-12-02 14:35:24 +0100

    Implement mission activity to show all available missions and upload one.

--- app/src/main/java/ch/bfh/ti/these/msp/ui/MissionActivity.java
+++ app/src/main/java/ch/bfh/ti/these/msp/ui/MissionActivity.java
@@ -1,49 +1,50 @@
 package ch.bfh.ti.these.msp.ui;
 
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import ch.bfh.ti.these.msp.R;
-import ch.bfh.ti.these.msp.mavlink.MavlinkConnectionInfo;
-import ch.bfh.ti.these.msp.mavlink.MavlinkMessageListener;
-import io.dronefleet.mavlink.MavlinkMessage;
 
-import static ch.bfh.ti.these.msp.MspApplication.getMavlinkMaster;
+public class MissionActivity extends FragmentActivity {
 
-
-public class MissionActivity extends AppCompatActivity implements MavlinkMessageListener {
-
-    // This event fires 2nd, before views are created for the fragment
-    // The onCreate method is called when the Fragment instance is being created, or re-created.
-    // Use onCreate for any standard setup that does not require the activity to be fully created
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission);
-
-        setupView();
-        getMavlinkMaster().addMessageListener(this);
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        getMavlinkMaster().removeMessageListener(this);
-    }
+    /*private class DownloadMissionResult extends AsyncTask<Void, Void, Boolean> {
 
-    @Override
-    public void messageReceived(MavlinkMessage message) {
-
-    }
-
-    @Override
-    public void connectionStatusChanged(MavlinkConnectionInfo info) {
-
-    }
-
-    private void setupView() {
+        @Override
+        protected Boolean doInBackground(Void... voids) {
 
 
-    }
+            try {
+                setStatusBusy(true);
+                hasFiles = true;
+                int index = 1;
+                while (hasFiles) {
+                    CompletableFuture cf = getMavlinkMaster().getFtpService().downloadFile("wp" + index++ + ".json").thenAccept(a -> {
 
+                        System.out.println(a);
+                        // TODO store to backend
+
+
+                    }).exceptionally(throwable -> {
+                        setStatusBusy(false);
+                        if (throwable != null) {
+                            hasFiles = false;
+                            showToast(throwable.getMessage());
+                        }
+                        return null;
+                    });
+
+                    cf.get();
+                }
+                showToast("download beendet");
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+            return true;
+        }
+    }*/
 }
---
+++ app/src/main/java/ch/bfh/ti/these/msp/ui/MissionDetailFragment.java
@@ -0,0 +1,81 @@
+package ch.bfh.ti.these.msp.ui;
+
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import ch.bfh.ti.these.msp.R;
+import ch.bfh.ti.these.msp.mavlink.model.Converter;
+import ch.bfh.ti.these.msp.mavlink.model.MavlinkMission;
+import ch.bfh.ti.these.msp.models.Mission;
+import ch.bfh.ti.these.msp.viewmodel.MissionRepository;
+import ch.bfh.ti.these.msp.viewmodel.MissionViewModel;
+
+import java.io.IOException;
+
+import static ch.bfh.ti.these.msp.MspApplication.getMavlinkMaster;
+
+public class MissionDetailFragment extends Fragment {
+
+    private MissionViewModel viewModel;
+    private TextView tvName;
+    private Button btnSelectMission;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mission_detail, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FragmentActivity activity = getActivity();
+        if (activity != null) {
+            viewModel = ViewModelProviders.of(activity).get(MissionViewModel.class);
+            viewModel.getSelectedMission().observe(this, this::updateDetails);
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvName = view.findViewById(R.id.tv_mission_name);
+        tvName.setVisibility(View.INVISIBLE);
+        btnSelectMission = view.findViewById(R.id.btn_select_mission);
+        btnSelectMission.setVisibility(View.INVISIBLE);
+        btnSelectMission.setOnClickListener(
+                v -> viewModel.downloadWayPoints().observe(this, this::uploadMavlinkMission)
+        );
+    }
+
+    private void updateDetails(Mission m) {
+        tvName.setText(m.getName());
+        showMission();
+    }
+
+    private void showMission() {
+        tvName.setVisibility(View.VISIBLE);
+        btnSelectMission.setVisibility(View.VISIBLE);
+    }
+
+    private void uploadMavlinkMission(MissionRepository.Result<Mission> result) {
+        if (result.getStatus()) {
+            MavlinkMission mission = Converter.convertToUploadItems(result.getPayload());
+            try {
+                getMavlinkMaster().getMissionService().uploadMission(mission).thenAccept((a) -> {
+
+                }).exceptionally(null);
+            } catch (IOException e) {
+                // @TODO SetStatus
+            }
+        }
+    }
+}
