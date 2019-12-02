21b0041a0470ca92b249f897e99dac8a4c5000a4
Samuel Ackermann <saemi.ackermann@gmail.com>
2019-12-02 14:47:38 +0100

    Add new activity

---
+++ app/src/main/java/ch/bfh/ti/these/msp/ui/MissionDownloadActivity.java
@@ -0,0 +1,14 @@
+package ch.bfh.ti.these.msp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import ch.bfh.ti.these.msp.R;
+
+public class MissionDownloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission_download);
+    }
+}
